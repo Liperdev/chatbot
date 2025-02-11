f3d8f64073dde99e182ca9f1cb98955f6f7fabdb
Felipe <felipe.devsoftwareengineer@gmail.com>
2025-02-10 21:34:19 -0300

    Adição de intervalos

--- src/chatbot.java
+++ src/chatbot.java
@@ -45,6 +45,7 @@ public class chatbot {
         String reclamacao = "";
         String maisReclamacao = "";
         String exibirReclamacao;
+        String submeterReclamacao;
 
         clientes.add("Manuel");
         clientes.add("Junior");
@@ -72,9 +73,15 @@ public class chatbot {
                     consultaCliente = entrada.next();
 
                     if (registros(clientes, consultaCliente)) {
+                        Thread.sleep(2000); 
                         System.out.println("Cliente encontrado: " + consultaCliente);
+                        Thread.sleep(2000); 
+                        System.out.println("Registro confirmado!");
+                        Thread.sleep(3000); 
+                        break;
                     } else {
                         System.out.println("Cliente não encontrado.");
+                        Thread.sleep(2000); 
                         System.out.println("[Sim ou Não] Gostaria de adicioná-lo? ");
                         opcaoAdicionar = entrada.next();
 
@@ -83,23 +90,31 @@ public class chatbot {
                                 System.out.print("Adicionar cliente: ");
                                 adicaoCliente = entrada.next();
                                 clientes.add(adicaoCliente);
+                                Thread.sleep(2000); 
                                 System.out.println("Cliente adicionado!");
 
+                                Thread.sleep(1000); 
                                 System.out.print("Gostaria de adicionar mais clientes? [Sim ou Não]: ");
                                 adicaoMaisCliente = entrada.next();
                             } while (adicaoMaisCliente.equalsIgnoreCase("sim"));
 
+                            Thread.sleep(2000); 
                             System.out.println("Lista de clientes: " + clientes);
+                            Thread.sleep(2000); 
                             System.out.print("Encerrar o registro? [Sim ou Não]: ");
                             encerrarRegistro = entrada.next();
 
                             if (encerrarRegistro.equalsIgnoreCase("sim")) {
                                 System.out.println("Encerrando...");
+                                Thread.sleep(2000); 
+                                System.out.println("Encerrado!");
                                 return;
                             }
                         } else {
+                            Thread.sleep(2000); 
                             System.out.println("Retornando ao menu principal...");
-                            return;
+                            Thread.sleep(1000); 
+                            break;
                         }
                     }
                     break;
@@ -112,6 +127,7 @@ public class chatbot {
 
                     while (opcaoSuporte < 1 || opcaoSuporte > 3) {
                         System.out.println("Opção inexistente!");
+                        Thread.sleep(2000);
                         System.out.print("Opção: ");
                         opcaoSuporte = entrada.nextInt();
                     }
@@ -119,17 +135,25 @@ public class chatbot {
                     switch (opcaoSuporte) {
                         case 1:
                             System.out.println("Dúvida ao entrar na plataforma!");
+                            Thread.sleep(2000);
                             System.out.print("Digite sua dúvida: ");
                             duvidaPlataforma = entrada.next();
 
+                            Thread.sleep(2000);
+                            System.out.println("Sua dúvida: " + duvidaPlataforma);
                             agradecimento();
                             break;
 
                         case 2:
                             System.out.println("Solicitação de visita!");
+                            Thread.sleep(1000);
                             System.out.print("(ex: 01/01/2025) Digite a data: ");
                             dataVisita = entrada.next();
+                            Thread.sleep(2000);
                             System.out.print("Data desejada: " + dataVisita);
+                            Thread.sleep(1500);
+                            System.out.println("Analisando data...");
+                            Thread.sleep(2000);
                             System.out.println("Data confirmada!");
 
                             agradecimento();
@@ -148,11 +172,14 @@ public class chatbot {
 
                     while (duvidaCliente.equals("")) {
                         System.out.println("Campo vazio, digite algo!");
+                        Thread.sleep(1000);
                         System.out.print("Digite sua dúvida: ");
                         duvidaCliente = entrada.nextLine();
                     }
 
+                    Thread.sleep(1500);
                     System.out.println("Dúvida registrada!");
+                    Thread.sleep(1500);
                     agradecimento();
 
                     break;
@@ -164,11 +191,18 @@ public class chatbot {
 
                     if (compras(compras, consultaCompra)) {
                         System.out.println("Compra encontrada: " + consultaCompra);
+                        Thread.sleep(2000);
                         System.out.println("Chegará em poucos dias! ");
+                        Thread.sleep(1500);
                         System.out.println("Aguarde o recebimento em breve da sua compra em breve! ");
+                        Thread.sleep(2000);
                         System.out.println("Finalizando...");
+                        Thread.sleep(2000);
+                        System.out.println("Finalizado!");
+                        return;
                     } else {
                         System.out.println("Compra não encontrada!");
+                        Thread.sleep(1500);
                         System.out.println("[Sim ou Não] Você gostaria de adicionar uma compra? ");
                         produtoAdicionar = entrada.next();
 
@@ -177,9 +211,11 @@ public class chatbot {
                             produto = entrada.next();
                             compras.add(produto);
 
-                            System.out.println("[Sim ou Não] Você gostaria de adicionar uma compra? ");
+                            Thread.sleep(1500);
+                            System.out.println("[Sim ou Não] Você gostaria de adicionar mais uma compra? ");
                             produtoAdicionar = entrada.next();
                         }
+
 
                         System.out.println("Deseja exibir a lista de compras? ");
                         exibirProduto = entrada.next();
@@ -188,7 +224,10 @@ public class chatbot {
                             System.out.println(compras);
                         } else {
                             agradecimento();
-                            System.out.println("Encerrando...");
+                            Thread.sleep(2000);
+                            System.out.println("Finalizando...");
+                            Thread.sleep(1700);
+                            System.out.println("Finalizado!");
                             break;
                         }
                     }
@@ -197,10 +236,12 @@ public class chatbot {
                 case 5:
                     System.out.println("Você escolheu => Setor de Reclamações");
                     System.out.println("Deixe sua reclamação aqui!");
+                    Thread.sleep(1000);
                     System.out.print("Reclamação: ");
                     reclamacao = entrada.nextLine().trim();
                     feedback.add(reclamacao);
 
+                    Thread.sleep(1500);
                     System.out.print("[Sim ou Não] Gostaria de enviar outra reclamação? ");
                     maisReclamacao = entrada.nextLine().trim();
 
@@ -209,16 +250,36 @@ public class chatbot {
                         System.out.print("Reclamação: ");
                         reclamacao = entrada.nextLine().trim();
                         feedback.add(reclamacao);
+                        Thread.sleep(1500);
                         System.out.println("[Sim ou Não] Gostaria de enviar outra reclamação? ");
                         maisReclamacao = entrada.nextLine().trim();
 
                     }
-                    System.out.println("Sua lista de reclamações: ");
+                    Thread.sleep(1000);
+                    System.out.println("Exibindo lista de reclamações...");
+                    Thread.sleep(2000);
                     System.out.println(feedback);
+                    System.out.print("[Sim ou Não] Gostaria de enviá-la? ");
+                    submeterReclamacao = entrada.next();
+                    
+                    if (submeterReclamacao.equalsIgnoreCase("Sim")) {
+                        Thread.sleep(2000);
+                        System.out.println("Enviando...");
+                        Thread.sleep(1600);
+                        System.out.println("Reclamação enviada!");
+                        Thread.sleep(1400);
+                        agradecimento();
+                    } else {
+                        System.out.println("Finalizando...");
+                        Thread.sleep(2000);
+                        System.out.println("Finalizado!");
+                    }
                     break;
 
                 case 6:
                     System.out.println("Finalizando...");
+                    Thread.sleep(2000);
+                    System.out.println("Finalizado!");
                     return;
 
                 default:
